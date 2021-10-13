cdd605748050072f5c3864a35a27f8f04e572c15
Yatik Vansiya <yvansiya@gmail.com>
2021-10-13 14:26:24 +0530

    Git Test- 2 Student Model Add ContactNumber

--- Yatik_Task4/Models/StudentModel.cs
+++ Yatik_Task4/Models/StudentModel.cs
@@ -4,5 +4,6 @@ namespace Yatik_Task4.Models
     {
         public string Name { get; set; }     
         public string Address { get; set; }  
+        public string ContactNumber { get; set; }  
     }
 }

02b02135a1b0838dd73ad924121063c565b690f5
Yatik Vansiya <yvansiya@gmail.com>
2021-10-13 15:13:03 +0530

    New changes

--- Yatik_Task4/Models/StudentModel.cs
+++ Yatik_Task4/Models/StudentModel.cs
@@ -2,6 +2,8 @@ namespace Yatik_Task4.Models
 {
     public class StudentModel
     {
-        
+        public string Name { get; set; }     
+        public string Address { get; set; }  
+        public string Contact { get; set; }  
     }
 }

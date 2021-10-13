7035eaa6624124aafc63303abf75bce0dbe2236e
Yatik Vansiya <yvansiya@gmail.com>
2021-10-13 14:54:54 +0530

    Student Model Remove All Changes

--- Yatik_Task4/Models/StudentModel.cs
+++ Yatik_Task4/Models/StudentModel.cs
@@ -2,8 +2,6 @@ namespace Yatik_Task4.Models
 {
     public class StudentModel
     {
-        public string Name { get; set; }     
-        public string Address { get; set; }  
-        public string ContactNumber { get; set; }  
+        
     }
 }
